68ddd55654197d153b4d120653d0002db7be2fbd
OxSuat <suat.izmir@doctolib.com>
2024-04-24 11:41:08 +0200

    creation du composant timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponentComponent } from './timer-component/timer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponentComponent
   ],
   imports: [
     BrowserModule,
